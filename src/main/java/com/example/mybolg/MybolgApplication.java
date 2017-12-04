879637a45ec443e6aa8ae8346daba4cc24e7f4cf
linxidream <362499773@qq.com>
2017-12-04 09:58:01 +0800

    初始化

---
+++ src/main/java/com/example/mybolg/MybolgApplication.java
@@ -0,0 +1,12 @@
+package com.example.mybolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybolgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybolgApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/mybolg/MybolgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mybolg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybolgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
